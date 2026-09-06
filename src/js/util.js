8e1e45b390d061fa9b88f3bbf41a5ba7892d0227
Hung-Te Lin (piaip) <hungte@gmail.com>
2026-09-07 03:23:18 +0800

    refactor(dom): Modernize deprecated DOM APIs
    Including query parsing and remove global React.

--- src/js/util.js
+++ src/js/util.js
@@ -13,13 +13,7 @@ export function setTimer(repeat, func, timelimit) {
 }
 
 export function getQueryVariable(variable) {
-  const query = window.location.search.substring(1);
-  const vars = query.split("&");
-  for (let i = 0; i < vars.length; i++) {
-    const pair = vars[i].split("=");
-    if (pair[0] === variable) {
-      return pair.length > 1 && pair[1] ? decodeURIComponent(pair[1]) : null;
-    }
-  }
-  return null;
+  if (typeof window === "undefined" || !window.location) return null;
+  const params = new URLSearchParams(window.location.search);
+  return params.get(variable);
 }
