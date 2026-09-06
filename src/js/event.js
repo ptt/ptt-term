9582ecfb227df72d9665872a4bf5fae308c06f1f
Hung-Te Lin (piaip) <hungte@gmail.com>
2026-09-06 17:59:31 +0800

    refactor(event): Convert Event to ES6 class

--- src/js/event.js
+++ src/js/event.js
@@ -1,36 +1,39 @@
-export function Event() {}
-
-Event.mixin = function(obj) {
-  for (var key in Event.prototype) {
-    obj[key] = Event.prototype[key];
-  }
-};
-
-Event.prototype.addEventListener = function(type, listener) {
-  this._listeners = this._listeners || {};
-  (this._listeners[type] = this._listeners[type] || []).push(listener);
-};
-
-Event.prototype.dispatchEvent = function(e) {
-  this._listeners = this._listeners || {};
-  var fns = this._listeners[e.type];
-  if (fns) {
-    fns = fns.slice(0);
-    for (var i in fns) {
-      fns[i](e);
-    }
-  }
-};
-
-Event.prototype.removeEventListener = function(type, listener) {
-  this._listeners = this._listeners || {};
-  var fns = this._listeners[type];
-  if (fns) {
-    for (var i in fns) {
-      if (fns[i] === listener) {
-        fns.splice(i, 1);
-        break;
+export class Event {
+  static mixin(obj) {
+    const proto = Event.prototype;
+    for (const key of Object.getOwnPropertyNames(proto)) {
+      if (key !== 'constructor') {
+        obj[key] = proto[key];
       }
     }
   }
-};
+
+  addEventListener(type, listener) {
+    this._listeners = this._listeners || {};
+    (this._listeners[type] = this._listeners[type] || []).push(listener);
+  }
+
+  dispatchEvent(e) {
+    this._listeners = this._listeners || {};
+    var fns = this._listeners[e.type];
+    if (fns) {
+      fns = fns.slice(0);
+      for (var i in fns) {
+        fns[i](e);
+      }
+    }
+  }
+
+  removeEventListener(type, listener) {
+    this._listeners = this._listeners || {};
+    var fns = this._listeners[type];
+    if (fns) {
+      for (var i in fns) {
+        if (fns[i] === listener) {
+          fns.splice(i, 1);
+          break;
+        }
+      }
+    }
+  }
+}
