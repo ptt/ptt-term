07e53adb6c0b97e1f8e0e4945d329cebbbafeed9
Hung-Te Lin (piaip) <hungte@gmail.com>
2026-09-07 03:23:43 +0800

    refactor(events): Inherit from native EventTarget

--- src/js/event.js
+++ src/js/event.js
@@ -1,39 +1,32 @@
-export class Event {
-  static mixin(obj) {
-    const proto = Event.prototype;
-    for (const key of Object.getOwnPropertyNames(proto)) {
-      if (key !== 'constructor') {
-        obj[key] = proto[key];
-      }
-    }
-  }
-
-  addEventListener(type, listener) {
-    this._listeners = this._listeners || {};
-    (this._listeners[type] = this._listeners[type] || []).push(listener);
-  }
-
-  dispatchEvent(e) {
-    this._listeners = this._listeners || {};
-    const fns = this._listeners[e.type];
-    if (fns) {
-      const copy = fns.slice(0);
-      for (let i = 0; i < copy.length; i++) {
-        copy[i](e);
-      }
-    }
-  }
-
-  removeEventListener(type, listener) {
-    this._listeners = this._listeners || {};
-    const fns = this._listeners[type];
-    if (fns) {
-      for (let i = 0; i < fns.length; i++) {
-        if (fns[i] === listener) {
-          fns.splice(i, 1);
-          break;
+const BaseEventTarget =
+  typeof EventTarget !== "undefined"
+    ? EventTarget
+    : class FallbackEventTarget {
+        addEventListener(type, listener) {
+          this._listeners = this._listeners || {};
+          (this._listeners[type] = this._listeners[type] || []).push(listener);
         }
-      }
-    }
-  }
-}
+
+        dispatchEvent(e) {
+          this._listeners = this._listeners || {};
+          const fns = this._listeners[e.type];
+          if (fns) {
+            for (const fn of fns.slice(0)) {
+              fn(e);
+            }
+          }
+          return true;
+        }
+
+        removeEventListener(type, listener) {
+          this._listeners = this._listeners || {};
+          const fns = this._listeners[type];
+          if (fns) {
+            const idx = fns.indexOf(listener);
+            if (idx !== -1) fns.splice(idx, 1);
+          }
+        }
+      };
+
+export class Event extends BaseEventTarget {}
+export default Event;
